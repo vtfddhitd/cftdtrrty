3b3d0e3c8d3ca85718bcca2b494fe6ab3e87384b
vtfddhitd <126434643+vtfddhitd@users.noreply.github.com>
2023-02-27 03:00:45 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=9ca83637-e6dc-47bc-bc34-43a3074e4465
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=gameshedge.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
